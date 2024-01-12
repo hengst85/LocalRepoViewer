508c9664fcbf4138e3cf06eea80e87aa39929af3
Hengstmann, Stefan (S.) <shengstm@ford.com>
2024-01-12 07:18:35 +0100

    Add git and svn repo class

---
+++ CM/Git.py
@@ -0,0 +1,256 @@
+"""
+Extended functionality for Repo class from GitPython
+
+--------------------------------------------------------------------------------
+Import Modules """
+# standard modules
+import os
+import sys
+import json
+from git import Repo
+from zipfile import ZipFile
+from pathlib import Path
+from send2trash import send2trash
+
+# dSPACE modules
+
+# customer modules
+# Get config data
+from ConfigFileHandling import GetConfigData
+
+ConfigData = GetConfigData({"Tools": "Tools"}, printInfo=False)
+
+# dSPACE modules (for message handling)
+if os.path.exists(ConfigData["WFM"]["PythonApi"]):
+    if ConfigData["WFM"]["PythonApi"] not in sys.path:
+        sys.path.append(ConfigData["WFM"]["PythonApi"])
+    from WFM.Base import API
+
+    api = API()
+
+""" --------------------------------------------------------------------------------
+Import Modules"""
+
+
+class ExtendedGitRepo(Repo):
+    def getRemoteHeadRev(self, branch: str = None):
+        """Get revision from remote head(s)
+            https://cloudaffaire.com/faq/git-ls-remote-in-gitpython/
+
+        Args:
+            branch (str, optional):
+                If branch is given, only revision for this branch will be returned. Defaults to None.
+
+        Returns:
+            dict / str: Remote head revision for all or given branches
+        """
+        remote_refs = {}
+        url = next(self.remotes.origin.urls)
+        for ref in self.git.ls_remote("--heads", url, branch).split("\n"):
+            hash_ref_list = ref.split("\t")
+            remote_refs[hash_ref_list[1].split("/")[-1]] = hash_ref_list[0]
+        if "api" in globals():
+            api.sendDebugMessage(f"Remote references:\n{json.dumps(remote_refs, indent=4)}")
+        if branch is None:
+            return remote_refs
+        else:
+            return remote_refs[branch]
+
+
+    def getFirstCommit(self,branch:str = None) -> str:
+        """Get first revision for given branch
+
+        Args:
+            branch (str, optional): Branch name. Defaults to None.
+
+        Returns:
+            str: First revision sha
+        """
+        # get active branch name
+        if branch is None:
+            branch = self.active_branch.name
+        
+        if branch in self.branches:
+            firstCommitRev = self.branches[branch].repo.git.rev_list("--max-parents=0","HEAD")
+            if firstCommitRev:
+                firstCommitRev = firstCommitRev.split()[-1]
+            api.sendDebugMessage(f"First commit is '{firstCommitRev}' for branch {branch}.")
+            return firstCommitRev
+        else:
+            api.sendDebugMessage(f"{branch} does not exist in given repository.")
+            return None
+
+
+    def checkLocalRevExists(self, revision: str) -> bool:
+        """Check if given revision exists in local commits
+
+        Args:
+            revision (str): Revision to check
+
+        Returns:
+            bool: Check result
+        """
+        # // if revision in self.git.rev_list('HEAD').split('\n'):
+        if revision in (c.hexsha for c in self.iter_commits()):
+            if "api" in globals():
+                api.sendDebugMessage(f"Revision '{revision}' exists in local repository.")
+            return True
+        else:
+            if "api" in globals():
+                api.sendDebugMessage(f"Revision '{revision}' does not exist in local repository.")
+            return False
+
+
+    def setupCleanWorkCopy(self, revision: str = None, branch: str = None, backup: bool = False) -> str:
+        """Setup clean local branch (Reset and clean local data)
+            https://stackoverflow.com/questions/11864735/how-to-do-a-git-reset-hard-using-gitpython
+
+        Args:
+            revision (str, optional):
+                Revision to checkout. If None, head revision from remote will be used. Defaults to None.
+            branch (str, optional):
+                Branch to checkout. If None, active branch will be used. Defaults to None.
+            backup (bool, optional):
+                Flag to make backup. Defaults to False.
+
+        Returns:
+            str: active revision number
+        """
+        # get active branch name
+        if branch is None:
+            branch = self.active_branch.name
+        if "api" in globals():
+            api.sendDebugMessage(f"Selected branch is '{branch}'.")
+
+        # get head revision for given branch
+        if revision is None:
+            revision = self.getRemoteHeadRev(branch)
+        if "api" in globals():
+            api.sendDebugMessage(f"Selected revision is '{revision}'.")
+
+        # check for modifications and create backup
+        if backup and self.is_dirty(untracked_files=True):
+            # Zip folder (local work copy)
+            path = Path(self.working_dir)
+            backupFile = Path(path.parent, path.name + ".zip")
+            # Delete old zip file
+            backupFile.unlink(missing_ok=True)
+
+            # Get list of changed files
+            changedFiles = [item.a_path for item in self.index.diff(None)]
+            stagedFiles = [item.a_path for item in self.index.diff(branch)]
+            # Loop over all changed files
+            for file in set(stagedFiles + changedFiles + self.untracked_files):
+                file = Path(path, file)
+                if file.exists():
+                    with ZipFile(backupFile, "a") as zip:
+                        # zipping the file
+                        zip.write(file, file.relative_to(path))
+                    api.sendDebugMessage(f"File '{file.relative_to(path)}' is added to zip file!")
+                else:
+                    api.sendDebugMessage(f"File '{file.relative_to(path)}' is missing or deleted!")
+
+            # move to recycling bin
+            if backupFile.exists():
+                send2trash(backupFile)
+                api.sendDebugMessage(f"Backup '{backupFile}' is sent to trash!")
+
+        # check if commit exist in local repo
+        if not self.checkLocalRevExists(revision):
+            # fetch data from remote
+            result = self.remotes.origin.fetch(branch)
+            if "api" in globals():
+                api.sendDebugMessage(
+                    f"Data fetched up to commit '{result[0].commit.hexsha}' ({result[0].commit.committed_datetime})"
+                )
+
+        if "api" in globals():
+            api.sendDebugMessage(f"Cleanup local repository '{self.working_dir}'.")
+
+        # blast any current changes
+        result = self.git.reset("--hard")
+        if "api" in globals():
+            api.sendDebugMessage(f"{result}")
+
+        # ensure branch is checked out
+        if branch in self.heads:
+            result = self.heads[branch].checkout(force=True)
+            if "api" in globals():
+                api.sendDebugMessage(
+                    f"Checkout '{branch}' to commit '{result.commit.hexsha}' ({result.commit.committed_datetime})"
+                )
+        else:
+            result = self.git.checkout(branch, force=True)
+            if "api" in globals():
+                api.sendDebugMessage(f"{result}")
+
+        # blast any changes there (only if it wasn't checked out)
+        result = self.active_branch.repo.git.reset("--hard", revision)
+        if "api" in globals():
+            api.sendDebugMessage(f"{result}")
+
+        # remove any extra non-tracked files (.pyc, etc)
+        result = self.git.clean("-xdf")
+        result = result.replace("\n", "\n\t\t\t")
+        if result:
+            if "api" in globals():
+                api.sendDebugMessage(f"Cleanup data:\n\t\t\t{result}")
+
+        if "api" in globals():
+            api.sendDebugMessage(f"Clean working copy '{self.active_branch.name}' is ready to use!")
+
+        # return active revision
+        return self.active_branch.commit.hexsha
+
+
+    def generateChangelog(self, logFilePath: str, startCommit: str = None, items: list = []) -> None:
+        """Generate change log for given items 
+
+        Args:
+            logFilePath (str): Changelog file path
+            startCommit (str, optional): Commit to start with changelog. Defaults to None.
+            items (list, optional): List of files/folders for which a change log is to be created. Defaults to [].
+        """
+        # Create folder structure
+        logFilePath = Path(logFilePath)
+        logFilePath.parent.mkdir(parents=True, exist_ok=True)
+
+        if not self.checkLocalRevExists(startCommit):
+            # Get initial commit
+            startCommit = self.getFirstCommit()
+
+        # Filter items, whether item is part of the repo
+        items = [item for item in items if self.working_dir in item]
+
+        # Get changelog and write to file
+        changeLog = self.active_branch.repo.git.log("--no-merges", 
+                        "--format=%s%n========%nAuthor: %aN%nCommit: %H%nDate:   %aD%n%n%b%n", 
+                        f"{startCommit}..HEAD", items)
+        with open(logFilePath, "w") as logFile:
+            logFile.write(changeLog)
+            
+        
+    def getCommitSha(self, item: str) -> str:
+        # Get relative path of item
+        item = Path(item)
+        if item.is_relative_to(self.working_dir):
+            item = item.relative_to(self.working_dir)
+        
+        # Get last commit hash of file
+        hash = [commit.hexsha for commit in self.active_branch.repo.iter_commits(max_count=1, paths=item.as_posix())]
+        if hash:
+            return hash[0]
+        else:
+            return '' 
+
+
+    def getFileSha(self, item: str) -> str:
+        # Get relative path of item
+        item = Path(item)
+        if item.is_relative_to(self.working_dir):
+            item = item.relative_to(self.working_dir)
+        
+        # Get repo tree object    
+        tree = self.heads[self.active_branch.name].commit.tree
+        
+        return tree[item.as_posix()].hexsha
---
+++ CM/Svn.py
@@ -0,0 +1,211 @@
+"""
+Extended functionality for Repository class from WFM SVN 
+
+--------------------------------------------------------------------------------
+Import Modules """
+# standard modules
+import os
+import sys
+import clr
+from zipfile import ZipFile
+from pathlib import Path
+from send2trash import send2trash
+
+# customer modules
+# Get config data
+from ConfigFileHandling import GetConfigData
+
+ConfigData = GetConfigData({"Tools": "Tools"}, printInfo=False)
+
+# dSPACE modules (for message handling, etc.)
+if os.path.exists(ConfigData["WFM"]["PythonApi"]):
+    if ConfigData["WFM"]["PythonApi"] not in sys.path:
+        sys.path.append(ConfigData["WFM"]["PythonApi"])
+    from WFM.Base import API
+    from WFM.CM.Svn import Repository
+
+    api = API()
+
+if os.path.realpath(os.path.join(ConfigData["WFM"]["PythonApi"], "Libraries")) not in sys.path:
+    sys.path.append(os.path.realpath(os.path.join(ConfigData["WFM"]["PythonApi"], "Libraries")))
+clr.AddReference("SharpSvn")
+import SharpSvn  # noqa: E402
+
+""" --------------------------------------------------------------------------------
+Import Modules"""
+
+
+class ExtendedSvnRepo(Repository):
+    def _getStatus(self):
+        """Get status of local working copy
+
+        Returns:
+            object:
+        """
+        # Set client arguments
+        statusArgs = SharpSvn.SvnStatusArgs()
+        statusArgs.RetrieveAllEntries = True
+        # // statusArgs.Depth = SharpSvn.SvnDepth.Infinity
+        # // statusArgs.IgnoreWorkingCopyStatus = True
+        # Get status
+        return self._svnClient.GetStatus(self._localWorkingFolder, statusArgs, None)
+
+    def is_dirty(self, untracked_files: bool = True) -> bool:
+        """Check if local working copy is dirty
+
+        Args:
+            untracked_files (bool, optional): Check for untracked files. Defaults to True.
+
+        Returns:
+            bool: True if local working copy is dirty
+        """
+        # https://sharpsvntips.net/post/45301419716/getstatus-and-status
+        # https://stackoverflow.com/questions/26218776/what-is-the-meaning-of-the-svn-statuses-contentstatus-nodestatus-propertystatu
+
+        # get status of local working copy
+        status = self._getStatus()
+
+        # Check for dirty status (set is much faster than list)
+        dirtyStatusList = {
+            SharpSvn.SvnStatus.Modified,
+            SharpSvn.SvnStatus.Added,
+            SharpSvn.SvnStatus.Missing,
+            SharpSvn.SvnStatus.Deleted,
+            SharpSvn.SvnStatus.Conflicted,
+        }
+        if untracked_files:
+            dirtyStatusList.add(SharpSvn.SvnStatus.NotVersioned)
+        return any([x.LocalContentStatus in dirtyStatusList for x in status[1]])
+
+        # //# Check for normal status
+        # //cleanStatus = SharpSvn.SvnStatus.Normal
+        # //return not all([x.LocalContentStatus == cleanStatus for x in status[1]])
+
+    def getWorkCopyInfo(self) -> dict:
+        """Get info of local working copy
+
+        Returns:
+            dict: Dictionary with status information
+        """
+        # Get information for given local working directory
+        target = SharpSvn.SvnPathTarget(self._localWorkingFolder)
+        info = self._svnClient.GetInfo(target, None)
+
+        # Get branch name, work copy status, ...
+        status = {}
+        status["branch"] = Path(info[1].Uri.AbsoluteUri).relative_to(self._repositoryUrl)
+        status["repoUrl"] = Path(self._repositoryUrl)
+        status["isDirty"] = self.is_dirty()
+        # //status["revision"] = str(info[1].LastChangeRevision)
+        status["revision"] = str(info[1].Revision)
+
+        return status
+
+    def getLocalRevision(self) -> str:
+        """Get revision from local working copy
+
+        Returns:
+            str: revision number
+        """
+        # Get information for given local working directory
+        target = SharpSvn.SvnPathTarget(self._localWorkingFolder)
+        info = self._svnClient.GetInfo(target, None)
+
+        # Return last change revision for given working directory
+        # //return str(info[1].LastChangeRevision)
+        return str(info[1].Revision)
+
+    def getRemoteHeadRevision(self) -> str:
+        """Get revision from remote repository folder
+
+        Returns:
+            str: revision number
+        """
+        # Get information for given repository folder from remote
+        target = SharpSvn.SvnUriTarget(self._repositoryFolder)
+        info = self._svnClient.GetInfo(target, None)
+
+        # Return last change revision for given repository folder
+        # //return str(info[1].LastChangeRevision)
+        return str(info[1].Revision)
+
+    def setupCleanWorkCopy(self, revision: str = None, branch: str = None, backup: bool = False) -> str:
+        """Setup clean local working copy (Reset and clean local data)
+
+        Args:
+            revision (str, optional):
+                Revision to checkout. If None, head revision from remote will be used. Defaults to None.
+            branch (str, optional):
+                Branch to checkout. If None, active branch will be used. Defaults to None.
+            backup (bool, optional):
+                Flag to make backup. Defaults to False.
+
+        Returns:
+            str: active revision number
+        """
+
+        # Get info of current work copy
+        info = self.getWorkCopyInfo()
+
+        # get active branch name
+        if branch is None:
+            branch = info["branch"].as_posix()
+        api.sendDebugMessage(f"Selected branch is '{branch}'.")
+
+        # get head revision for given branch
+        if revision is None:
+            revision = self.getRemoteHeadRevision()
+        api.sendDebugMessage(f"Selected revision is '{revision}'.")
+
+        # check for modifications and create backup
+        if backup and info["isDirty"]:
+            # Zip folder (local work copy)
+            path = Path(self._localWorkingFolder)
+            backupFile = Path(path.parent, path.name + ".zip")
+            # Delete old zip file
+            backupFile.unlink(missing_ok=True)
+
+            # Get work copy status
+            repoStatus = self._getStatus()
+            dirtyStatusList = {
+                SharpSvn.SvnStatus.Modified,
+                SharpSvn.SvnStatus.Added,
+                SharpSvn.SvnStatus.Missing,
+                SharpSvn.SvnStatus.Deleted,
+                SharpSvn.SvnStatus.Conflicted,
+                SharpSvn.SvnStatus.NotVersioned,
+            }
+
+            for item in (x for x in repoStatus[1] if x.LocalContentStatus in dirtyStatusList):
+                file = Path(item.FullPath)
+                if file.exists():
+                    with ZipFile(backupFile, "a") as zip:
+                        # zipping the file
+                        zip.write(file, file.relative_to(path))
+                    api.sendDebugMessage(f"File '{file.relative_to(path)}' is added to zip file!")
+                else:
+                    api.sendDebugMessage(f"File '{file.relative_to(path)}' is missing or deleted!")
+
+            # move to recycling bin
+            if backupFile.exists():
+                send2trash(backupFile)
+                api.sendDebugMessage(f"Backup '{backupFile}' is sent to trash!")
+
+        # revert changes
+        self.revert()
+        api.sendDebugMessage("Changes are reverted!")
+        # delete unversioned files
+        self.deleteUnversionedFiles()
+        api.sendDebugMessage("Unversioned files are deleted!")
+
+        # ensure branch is checked out
+        if branch != info["branch"].as_posix():
+            self.switchToBranch(branch)
+            api.sendDebugMessage(f"Switched to branch '{branch}'!")
+
+        # update to given revision
+        activeRevision = self.update(revision)
+        api.sendDebugMessage(f"Clean working copy '{branch}' is ready to use!")
+
+        # return active revision
+        return activeRevision
