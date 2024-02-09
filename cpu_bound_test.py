f58a8bdb65830ef9b177e98db2fbed1ebba35b72
hengst85 <149901964+hengst85@users.noreply.github.com>
2024-02-09 15:22:17 +0100

    cpu_bound_test.py erstellt

---
+++ cpu_bound_test.py
@@ -0,0 +1,52 @@
+from nicegui import ui, run
+import time
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+
+def taskA(txt: int):
+    print(f"taskA: {txt}")
+    time.sleep(1)
+    return txt
+    
+def taskB(txt: int):
+    print(f"taskB: {txt}")
+    time.sleep(1)
+    return txt
+    
+def call_taskA(x):
+    print("Tasks A will be executed")
+    results = []
+    with ThreadPoolExecutor as executor:
+        for result in executor.map(taskA, x):
+            results.append(result)
+            
+    return results
+    
+def call_taskB(x):
+    print("Tasks B will be executed")
+    results = []
+    with ThreadPoolExecutor as executor:
+        for result in executor.map(taskB, x):
+            results.append(result)
+
+    return results
+
+async def run_taskA(x):
+    print("Task A executor will be started")
+    result = await run.cpu_bound(call_taskA, x)
+    print(f"Task A finished: {result}")
+
+
+async def run_taskB(x):
+    print("Task B executor will be started")
+    result = await run.cpu_bound(call_taskB, x)
+    print(f"Task B finished: {result}")
+
+
+    
+ui.button("TaskA", on_click= lambda: run_taskA(range(5)))
+ui.button("TaskB", on_click= lambda: run_taskB(range(5)))
+    
+ui.run()
+    
+
