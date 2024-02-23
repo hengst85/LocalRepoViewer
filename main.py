304d1195148a15078522add2300eb0f470f44da8
hengst85 <hengst85@hotmail.com>
2024-02-23 22:59:10 +0100

    Add ui.page() to move UI definition from the global scope into a page function

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from nicegui import ui
 from pathlib import Path
 from repo_viewer import repo_viewer
 
-CONFIGFILE = Path(__file__).parent.joinpath('config.toml')
+CONFIGFILE = Path(__file__).parent.joinpath('config_private.toml')
 
 def main() -> None:
 
-    ui.colors(primary='#022b61', secondary='#555555', accent='#111B1E', positive='#53B689')
+    @ui.page('/')
+    def frame():  
+        ui.colors(primary='#022b61', secondary='#555555', accent='#111B1E', positive='#53B689')
 
-    with ui.column().classes('w-full'):
-        repo_viewer(CONFIGFILE)
+        with ui.column().classes('w-full'):
+            repo_viewer(CONFIGFILE)
 
     ui.run(reload=False, \
         show=False, \
@@ -18,5 +20,7 @@ def main() -> None:
         host='localhost',
         port=8083)
     #ui.run(reload=True)
+    
+    
 if __name__ in {'__main__', '__mp_main__'}:
     main()
