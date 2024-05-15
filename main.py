bfd3034d263eceaf996c528b0b3922d573fd31d4
Hengstmann, Stefan (S.) <shengstm@ford.com>
2024-05-15 07:04:24 +0200

    Delete spaces

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from repo_viewer import repo_viewer
 def main(configFile: str) -> None:
 
     @ui.page('/')
-    def frame():  
+    def frame():
         ui.colors(primary='#022b61', secondary='#555555', accent='#111B1E', positive='#53B689')
 
         with ui.column().classes('w-full'):
@@ -19,17 +19,16 @@ def main(configFile: str) -> None:
         host='localhost',
         port=8083)
     #ui.run(reload=True)
-    
-    
+
+
 if __name__ in {'__main__', '__mp_main__'}:
     import argparse
 
     parser = argparse.ArgumentParser(description="Start Repository Viewer")
     parser.add_argument("ConfigFile", type=str, help="Path to config file.")
     args = parser.parse_args()
-    
+
     main(args.ConfigFile)
-    
+
     #main(Path(__file__).parent.joinpath('config_private.toml'))
     #main(Path(__file__).parent.joinpath('config.toml'))
-    
